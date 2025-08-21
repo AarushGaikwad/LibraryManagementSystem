b8f79312700ff30363dad4fb2e62899a40a198de
AarushGaikwad <Aarushgaikwad789@gmail.com>
2025-08-21 11:41:58 +0530

    initial commit

--- backend/LibraryManagementSystem/src/main/java/com/example/LibraryManagementSystem/LibraryManagementSystemApplication.java
+++ backend/LibraryManagementSystem/src/main/java/com/example/LibraryManagementSystem/LibraryManagementSystemApplication.java
@@ -1,13 +1,13 @@
-package com.example.libraryManagementStstem;
+package com.example.LibraryManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryManagementStstemApplication {
+public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementStstemApplication.class, args);
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
 
 }
--- backend/LibraryManagementSystem/src/test/java/com/example/LibraryManagementSystem/LibraryManagementSystemApplicationTests.java
+++ backend/LibraryManagementSystem/src/test/java/com/example/LibraryManagementSystem/LibraryManagementSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.libraryManagementStstem;
+package com.example.LibraryManagementSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LibraryManagementStstemApplicationTests {
+class LibraryManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
