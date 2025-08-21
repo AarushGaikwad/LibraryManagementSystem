3a709cf7b424d9a193cfd678e70644eff18853a4
AarushGaikwad <Aarushgaikwad789@gmail.com>
2025-08-21 11:13:53 +0530

    initialize the project base

---
+++ backend/libraryManagementStstem/src/main/java/com/example/libraryManagementStstem/LibraryManagementStstemApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryManagementStstem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementStstemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementStstemApplication.class, args);
+	}
+
+}
